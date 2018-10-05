11bb89b7c0c35168970e674f8b3e03be84f632ba
Ana Harris <vana1990@gmail.com>
2018-10-05 04:30:38 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -5,8 +5,6 @@ def run_guessing_game
   input = gets.chomp
   unless input == "exit"
     number = rand(1..6)
-    puts "Input: #{input}"
-    puts "Number: #{number}"
     if input.to_i == number
       puts "You guessed the correct number!"
     else 
